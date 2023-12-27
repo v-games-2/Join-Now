2db00fa341423e956f8eec82947373f48d937559
v-games-2 <154949222+v-games-2@users.noreply.github.com>
2023-12-27 09:23:24 -0500

    Update script.js
    Signed-off-by: v-games-2 <154949222+v-games-2@users.noreply.github.com>

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function changeTitle() {
 function panic() {
     window.location.href = "https://example.com"; // Replace with your desired URL
 }
+
+// Listen for keydown events on the entire document
+document.addEventListener('keydown', function(event) {
+    if (event.key === '/') {
+        panic();
+    }
+});
+
+// Optional: Attach panic function to button for click as well
+document.getElementById("panicButton").onclick = panic;
