c248d6e0af644ad653aa738b1418ef119829cea2
v-games-2 <154949222+v-games-2@users.noreply.github.com>
2023-12-27 09:19:53 -0500

    Create script.js
    Signed-off-by: v-games-2 <154949222+v-games-2@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeTitle() {
+    var newTitle = document.getElementById("titleInput").value;
+    document.title = newTitle;
+}
+
+function panic() {
+    window.location.href = "https://example.com"; // Replace with your desired URL
+}
