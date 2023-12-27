1a09baafa29b16d4f4ad991bdfff0a2fcba6dc26
v-games-2 <154949222+v-games-2@users.noreply.github.com>
2023-12-27 09:37:47 -0500

    Create panic-button.js
    Signed-off-by: v-games-2 <154949222+v-games-2@users.noreply.github.com>

---
+++ panic-button.js
@@ -0,0 +1,5 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === '/') { // When '/' is pressed
+        window.location.href = 'https://example.com'; // Redirect to specified URL
+    }
+});
